b98f5821b18b2ae2ec3b0cea32299eefe08db5a5
Barr <barr520@gmail.com>
2024-03-25 22:04:37 +0200

    fix colormodel and write demo

--- color_model.py
+++ color_model.py
@@ -21,11 +21,25 @@ class UpConv(nn.Module):
         super(UpConv, self).__init__()
         self.convT = nn.ConvTranspose2d(in_channels, out_channels, 2, 2)
         self.conv1 = nn.Conv2d(in_channels, out_channels, (3, 3), padding=1)
+        self.relu = nn.ReLU()
 
     def forward(self, x, residual):
         x = self.convT(x)
         combined = torch.cat([x, residual], dim=1)
         out = self.conv1(combined)
+        out = self.relu(out)
+        return out
+
+
+class StayConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(StayConv, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, (3, 3), padding=1)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.relu(out)
         return out
 
 
@@ -36,22 +50,27 @@ class ColorNet(nn.Module):
         self.conv2 = DownConv(32, 64)  # 256 to 128
         self.conv3 = DownConv(64, 128)  # 128 to 64
         self.conv4 = DownConv(128, 256)  # 64 to 32
-        self.conv5 = DownConv(256, 512)  # 32 to 16
-        self.convT1 = UpConv(512, 256)  # 16 to 32
-        self.convT2 = UpConv(256, 128)  # 32 to 64
-        self.convT3 = UpConv(128, 64)  # 64 to 128
-        self.convT4 = UpConv(64, 32)  # 128 to 256
-        self.convout = nn.ConvTranspose2d(32, 2, 2, 2)
+        self.mid_conv = StayConv(256, 512)  # still 32
+        self.convT1 = UpConv(512, 256)  # 32 to 64
+        self.convT2 = UpConv(256, 128)  # 64 to 128
+        self.convT3 = UpConv(128, 64)  # 128 to 256
+        self.convT4 = UpConv(64, 32)  # 512 to 512
+        self.conv_out = nn.Conv2d(32, 2, 1, padding=1)  # 256 to 512
 
     def forward(self, x):
         x1 = self.conv1(x)
+        x1 = x1.view(-1, 32, 256, 256)
         x2 = self.conv2(x1)
+        x2 = x2.view(-1, 64, 128, 128)
         x3 = self.conv3(x2)
+        x3 = x3.view(-1, 128, 64, 64)
         x4 = self.conv4(x3)
-        x5 = self.conv5(x4)
+        x4 = x4.view(-1, 256, 32, 32)
+        x5 = self.mid_conv(x4)
+        x5 = x5.view(-1, 512, 16, 16)
         y1 = self.convT1(x5, x4)
         y2 = self.convT2(y1, x3)
         y3 = self.convT3(y2, x2)
         y4 = self.convT4(y3, x1)
-        out = self.convout(y4)
+        out = self.conv_out(y4)
         return out
---
+++ demo.py
@@ -0,0 +1,41 @@
+import torch
+from color_model import ColorNet
+from project import load_data
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+
+# parameters:
+train_ratio, epochs, batch_size, lab, checkpoint_interval = 0.9, 20, 32, True, 1
+# setup
+torch.manual_seed(0)
+device = torch.device("cpu")
+print(device)
+data, _ = load_data(1, lab)
+print("Loaded data")
+to_train = True
+model = ColorNet().to(device)
+model.load_state_dict(torch.load("model", map_location=device))
+model.eval()
+# model = torch.compile(model)
+fig = plt.figure(figsize=(1, 3))
+while True:
+    index = int(input(f"select image 1-{len(data)}: ")) + 1
+    image = data[index : index + 1]
+    fig.add_subplot(1, 3, 1)
+    image_in = Image.fromarray(np.asarray(image[0].permute(1, 2, 0)), "LAB").convert(
+        "RGB"
+    )
+    plt.imshow(image_in)
+    plt.title("Original")
+    fig.add_subplot(1, 3, 2)
+    plt.imshow(image[0, 0], cmap="gray")
+    plt.title("Grayscale")
+    fig.add_subplot(1, 3, 3)
+    model_out = model(image[:, 0].type(torch.float32)).type(torch.uint8)[0]
+    breakpoint()
+    image_out = Image.fromarray(
+        np.asarray(torch.cat([image[:, 0], model_out]).permute((1, 2, 0))), "LAB"
+    ).convert("RGB")
+    plt.imshow(image_out)
+    plt.show()
